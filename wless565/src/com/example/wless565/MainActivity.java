4ec0d5c2d95c549e604238857b8ec7f6715853a1
danielsalinasd <daniel.salinas@msu.montana.edu>
2014-11-26 22:00:20 -0700

    added post class

--- wless565/src/com/example/wless565/MainActivity.java
+++ wless565/src/com/example/wless565/MainActivity.java
@@ -7,6 +7,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.*;
 import org.apache.http.*;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
 
@@ -23,23 +24,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //set up client and create post request
-        HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost("ec2-54-148-149-145.us-west-2.compute.amazonaws.com");
+        //create a new class that executes a post request
+        PostStuff p = new PostStuff();
         
-        //create key-value pairs
-        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-        pairs.add(new BasicNameValuePair("k1", "v1"));
-        
-        //convert to post
-        try {
-			post.setEntity(new UrlEncodedFormEntity(pairs));
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
     }
-
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
